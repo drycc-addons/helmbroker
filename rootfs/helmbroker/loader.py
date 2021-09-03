174a6e76722b1fbb12cf09a90b8ba5a8cdc618da
duanhongyi <duanhongyi@doopai.com>
2021-09-03 17:15:17 +0800

    fix(helmbroker): dump_addons_meta not found

--- rootfs/helmbroker/loader.py
+++ rootfs/helmbroker/loader.py
@@ -4,7 +4,8 @@ import tarfile
 import requests
 import yaml
 
-from .config import ADDONS_PATH, CONFIG_PATH, dump_addons_meta
+from .config import ADDONS_PATH, CONFIG_PATH
+from .utils import dump_addons_meta
 
 
 def download_file(url, dest):
